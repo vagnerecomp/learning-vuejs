b535df1400a2a499e805cc277784a3c6deefbe3a
Vagner José <vagnerecomp@gmail.com>
2021-11-27 23:32:28 -0300

    Adicionando arquivos a branch main

---
+++ aula-vue3/src/app.js
@@ -0,0 +1,19 @@
+const MyNameApp = {
+    // A propriedade data retorna dados para o frontend da aplicação
+    data(){
+        return{
+            name: "Enéas",
+            age: 27,
+            input_name: ""
+        }        
+    },
+    methods:{
+        submitForm(e){
+            e.preventDefault();
+            // alert("chaama");
+            this.name  = this.input_name;
+        }
+    }
+}
+
+Vue.createApp(MyNameApp).mount("#app");
